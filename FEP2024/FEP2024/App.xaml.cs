39943383afcd29993b5d9bcbd0115546bc628d64
ISMAEL <ismaelsantanateixeira@gmail.com>
2024-11-08 19:16:10 -0300

    Criação da Tela de Cadastro.

--- FEP2024/FEP2024/App.xaml.cs
+++ FEP2024/FEP2024/App.xaml.cs
@@ -10,7 +10,7 @@ namespace FEP2024
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
