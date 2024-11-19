643c2e5d0f0448e6fa53b6ad2a31d15b1045efb5
ISMAEL <ismaelsantanateixeira@gmail.com>
2024-11-19 18:34:53 -0300

    Tela J Tem conta

---
+++ FEP2024/FEP2024/TelaJaPossuiCadastro.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FEP2024
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TelaJaPossuiCadastro : ContentPage
+	{
+		public TelaJaPossuiCadastro ()
+		{
+			InitializeComponent ();
+		}
+
+        
+
+        private void CriarContaBt_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new TelaCadastro());
+        }
+    }
+}
